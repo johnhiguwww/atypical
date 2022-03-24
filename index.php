f6ccacd56d600b4baf78b76d0e2c6e3725ef62dd
johnhiguwww <88637195+johnhiguwww@users.noreply.github.com>
2022-03-24 10:53:33 -0300

    commit atypical 2.1
    commit atypical 2.1

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
  <style>
    body{
       font-family:  Arial, Helvetica, sans-serif;
-   } 
+      background-image: linear-gradient(to right, rgb(255, 255, 0), rgb(0, 0, 255));
+    } 
    div{
       background-color: #304885;
       position: absolute;
@@ -31,35 +32,48 @@
   a{
     color: yellow;
   }
+{
+  margin: 0;
+  padding: 0;
+  font-family: 'Poppins', sans-serif
+}
+body
+{
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     min-height: 100vh;
+     background: #
+}
 
  </style>
 </head>
-<body background="./assets/img/fundo0.5.png">
-  
+<body">
+
 </body>
 </html>
 
 
   <a class="links" id="paracadastro"></a>
   <a class="links" id="paralogin"></a>
-  
+
     <div id="login">
       <img width="100%" src="./assets/img/iconpq.png" alt="png">
-    
+
       <form method="post" action=""> 
         <h1>Login</h1> 
         <p> 
-          <label for="email_login">Insira Seu Email</label>
-          <input id="email_login" name="email_login" required="required" type="text" placeholder="Email"/>
+          <label for="email_login">Email</label>
+          <input id="email_login" name="email_login" required="required" type="text" placeholder="Insira Seu Email"/>
         </p>
         <p> 
-          <label for="senha_login">Insira Sua Senha</label>
-          <input id="senha_login" name="senha_login" required="required" type="password" placeholder="Senha" /> 
-        </p>      
+          <label for="senha_login">Senha</label>
+          <input id="senha_login" name="senha_login" required="required" type="password" placeholder="Insira Sua Senha" /> 
+        </p>
         <p> 
           <input type="checkbox" name="manterlogado" id="manterlogado" value="" /> 
           <label for="manterlogado">Manter-me logado</label>
-        </p>      
+        </p>
         <p> 
           <input type="submit" value="Logar" /> 
         </p>
@@ -69,4 +83,3 @@
         </p>
       </form>
     </div>
-
--- to_ou_res.php
+++ to_ou_res.php
@@ -1,106 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="shortcut icon" type="imagex/png" href = "assets/iconTrello1.png">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tela de Cadastro</title>
-     <style>
-      body{       
-  margin: 0;
-  padding: 0;
-  font-family: 'poppins', sans-serif; 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh; 
-  }
-.container
-{
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-.container a
-{
-  position: relative;
-  width: 170px;
-  height: 60px;
-  text-align: center;
-  line-height: 55px;
-  margin: 10px;
-  text-transform: uppercase;
-  background: #191919;
-  color: #fff;
-  font-size: 18px;
-  font-weight: 500;
-  text-decoration: none;
-  letter-spacing: 2px;
-  overflow: hidden;
-}
-.container a span
-{
-  position: relative;
-  display: block;
-  width: 100%;
-  height: 100%;
-  z-index: 100000;
-  transition: 0s ease-in;
-}
-.container a:hover span 
-{
-transition: 0.4s ease-in;
-transform: translateY(-100%);
-}
-.container a:before
-{
-  content: '';
-  position: absolute;
-  bottom: -250px;
-  left: 60%;
-  transform: translateX(-59%);
-  width: 250px;
-  height: 250px;
-  background: #2299ff;
-  border-radius: 50%;
-  transition: 00.4s ease-in;
-}
-.container a:hover:before
-{
-  bottom: -150px;
-}
-.container a:nth-child(2):before
-{
-  bottom: 60px;
-}
-.container a:nth-child(2):hover:before
-{
-  bottom: -40px;
-}
-.terapeuta {
-  font-size: smaller;
-}
-     </style>
-    
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tela Cadastro</title>
+  <style>
+    body{
+      font-family: Arial, Helvetica, Sans-serif;
+      background-image: linear-gradient(to right, rgb(255, 255, 0), rgb(0, 0, 255));
+    }
+    div{
+      background-color: #304885;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+      padding: 120px;
+      border-radius: 15px;
+      color: white;
+    }
+    button{
+      background-color: white;
+      border: none;
+      padding: 15px;
+      width: 100%;
+      border-radius: 10px;
+      color: black;
+      font-size: 15px;
+      cursor: pointer;
+    }
+  </style>
 </head>
-   <body background="./assets/img/fundo0.5.png">
-    <div class="Cadastro">
-      </div class     >
-      <h1>CADASTRO</h1>
-       <h2> Você é?</h2>
-       <br><br>
-     <div class="container">
-        <a href="#">
-            <span>Terapeuta</span>
-            <span class="terapeuta">Iniciar cadastro Terapeuta</span>
-         </a>
-            <a href="./cadastrocrianca.php">
-              <span>Responsável</span>
-              <span>Iniciar cadastro Responsável</span>
-             </a>
-     </div>
+<body>
+<body>
+  
+    <div>
+      <h1>Cadastro</h1>
+      <H2>Você é?</H2>
+      <br><br>
+  <form action="cadastro_terapeuta.php">
+      <button type="submit">Terapeuta</button>
+      <br><br>
+      </form>
+      <form action="cadastrocrianca.php">
+      <button type="submit">Responsável</button>
+  </form>
+
     </div>
-   </body>
+
+</body>
 </html>
